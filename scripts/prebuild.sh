b5ed45b38c8fe47463a19e7d6a2bfd109cdd9f64
takdeveloper <66576793+takdeveloper@users.noreply.github.com>
2022-04-07 08:15:05 -0400

    try hard link also

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -54,13 +54,9 @@ pushd ../takthirdparty
 unlink android-armeabi-v7a-release
 unlink android-arm64-v8a-release
 unlink android-x86-release
-#ln -s builds/android-armeabi-v7a-release android-armeabi-v7a-release
-#ln -s builds/android-arm64-v8a-release android-arm64-v8a-release
-#ln -s builds/android-x86-release android-x86-release
-mv builds/android-armeabi-v7a-release .
-mv builds/android-arm64-v8a-release .
-mv builds/android-x86-release .
-
+ln builds/android-armeabi-v7a-release android-armeabi-v7a-release
+ln builds/android-arm64-v8a-release android-arm64-v8a-release
+ln builds/android-x86-release android-x86-release
 
 cd ci-support
 # install the packages locally
