9e64b1cba5af54979ad0da0cde70e86be1cbb27a
Uv <uv@crave.io>
2022-06-04 00:47:15 -0700

    use ninja

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -57,7 +57,7 @@ cd ci-support
 # conan
 conan export-pkg . -s arch=armv8 -s os=Android -s os.api_level=29 -f
 conan export-pkg . -s arch=armv7 -s os=Android -s os.api_level=29 -f
-conan export-pkg . -s arch=x86 -s os=Android -s os.api_level=29 -f
+conan export-pkg . -s arch=x86   -s os=Android -s os.api_level=29 -f
 
 # Install TTP maven package
 ./gradlew assemble
@@ -79,7 +79,7 @@ for LASZIP_ANDROID_ABI in ${ANDROID_ABIS} ;
 do
     mkdir -p build-android-${LASZIP_ANDROID_ABI}
     pushd build-android-${LASZIP_ANDROID_ABI}
-    cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK=${ANDROID_NDK_HOME} -DANDROID_ABI=${LASZIP_ANDROID_ABI} -DANDROID_TOOLCHAIN=gcc -DANDROID_STL=gnustl_static -DANDROID_PLATFORM=android-24 -DCMAKE_CXX_FLAGS="-fexceptions -frtti -std=c++11" -DLASZIP_BUILD_STATIC=ON
+    cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK=${ANDROID_NDK_HOME} -DANDROID_ABI=${LASZIP_ANDROID_ABI} -DANDROID_TOOLCHAIN=gcc -DANDROID_STL=gnustl_static -DANDROID_PLATFORM=android-24 -DCMAKE_CXX_FLAGS="-fexceptions -frtti -std=c++11" -DLASZIP_BUILD_STATIC=ON
     cmake --build .
     cp -r ../include .
     cp ../src/*.hpp ./include/laszip
@@ -89,7 +89,7 @@ done
 cd ci-support
 conan export-pkg . -s arch=armv8 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
 conan export-pkg . -s arch=armv7 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
-conan export-pkg . -s arch=x86 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
+conan export-pkg . -s arch=x86   -s os=Android -s os.api_level=29 -s compiler.version="8" -f
 
 popd
 
@@ -100,7 +100,7 @@ for LIBLAS_ANDROID_ABI in ${ANDROID_ABIS} ;
 do
     mkdir -p build-android-${LIBLAS_ANDROID_ABI}
     pushd build-android-${LIBLAS_ANDROID_ABI}
-    cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK=${ANDROID_NDK_HOME} -DANDROID_ABI=${LIBLAS_ANDROID_ABI} -DANDROID_TOOLCHAIN=gcc -DANDROID_STL=gnustl_static -DANDROID_PLATFORM=android-24 -DCMAKE_CXX_FLAGS="-fexceptions -frtti -std=c++11" -DLASZIP_BUILD_STATIC=ON
+    cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_NDK=${ANDROID_NDK_HOME} -DANDROID_ABI=${LIBLAS_ANDROID_ABI} -DANDROID_TOOLCHAIN=gcc -DANDROID_STL=gnustl_static -DANDROID_PLATFORM=android-24 -DCMAKE_CXX_FLAGS="-fexceptions -frtti -std=c++11" -DLASZIP_BUILD_STATIC=ON
     cmake --build . --target las_c
     cmake --build . --target las
     cp -r ../include .
@@ -111,7 +111,7 @@ cd ci-support
 # publish to conan
 conan export-pkg . -s arch=armv8 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
 conan export-pkg . -s arch=armv7 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
-conan export-pkg . -s arch=x86 -s os=Android -s os.api_level=29 -s compiler.version="8" -f
+conan export-pkg . -s arch=x86   -s os=Android -s os.api_level=29 -s compiler.version="8" -f
 
 # publish to maven
 ./gradlew assemble
